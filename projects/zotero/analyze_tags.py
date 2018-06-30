5eff161ac7e9f80358be35c12450e55a39598e67
zhanghaitao1 <13163385579@163.com>
2018-06-30 08:21:46 +0800

    update

--- projects/zotero/analyze_tags.py
+++ projects/zotero/analyze_tags.py
@@ -7,7 +7,7 @@
 
 from pyzotero import zotero
 
-library_id=412731
+library_id=4127316
 api_key='JpLmWYy5sIRuqbUwwZXDTfxm'
 
 zot=zotero.Zotero(library_id=library_id,library_type='user',api_key=api_key)
@@ -15,5 +15,22 @@ items=zot.top(limit=5)
 for item in items:
     print('Item: %s | key: %s'% (item['data'],item['data']['key']))
 
+
+
+zot.key_info()
+zot.groups()
+result=zot.everything(zot.top())
+
+dir(zot)
+
+
+tags=zot.tags()
+len(tags)
+
+zot.item_tags()
+
+tags=sorted(tags)
+tags
+#TOD    O:read sqlite directly from local disk
 
 
